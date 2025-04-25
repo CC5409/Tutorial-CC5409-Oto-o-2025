993e0cba939577f004343880177b9637781ca645
Elías Zelada <elixs.777@gmail.com>
2025-04-25 01:22:05 -0400

    removing health component scene

--- components/health_component.gd
+++ components/health_component.gd
@@ -1,5 +1,5 @@
 class_name HealthComponent
-extends Node
+extends MultiplayerSynchronizer
 
 
 signal died
@@ -8,21 +8,20 @@ signal health_changed(value)
 
 @export var health: int = 100:
 	set(value):
-		Debug.log(value)
 		if health == value:
 			return
 		health = clamp(value, 0 , max_health)
 		health_changed.emit(health)
 		if health == 0:
 			died.emit()
-
-
 @export var max_health: int = 100
 
 
-func _ready() -> void:
-	pass
+func _init() -> void:
+	replication_config.add_property("HealthComponent:health")
+	replication_config.property_set_replication_mode("HealthComponent:health", SceneReplicationConfig.REPLICATION_MODE_ALWAYS)
+	replication_config.property_set_spawn("HealthComponent:health", true)
+
 
 func take_damage(damage: int) -> void:
-	Debug.log(is_multiplayer_authority())
 	health -= damage
